70e02b67a3a4cb1ce81148580699822e6678bdcf
Darianlime <darianli447@gmail.com>
2025-10-17 02:25:08 -0500

    Add project files.

---
+++ RacingGame/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+int main() {
+	std::cout << "Welcome to the Racing Game!" << std::endl;
+	// Initialize game components here
+	return 0;
+}
